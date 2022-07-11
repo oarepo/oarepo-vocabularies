a0bf4788fc9d7c88a0c6f45fe77fc32713e025fd
Mirek Simek <miroslav.simek@gmail.com>
2022-07-11 20:34:30 +0200

    basic moved

--- oarepo-vocabularies/oarepo_vocabularies/basic/records/dumper.py
+++
@@ -1,9 +0,0 @@
-from invenio_records.dumpers import ElasticsearchDumper as InvenioElasticsearchDumper
-
-from oarepo_vocabularies.records.dumper import OARepoVocabularyDumperBase
-
-
-class OARepoVocabulariesBasicDumper(
-    OARepoVocabularyDumperBase, InvenioElasticsearchDumper
-):
-    """OARepoVocabularyBasic elasticsearch dumper."""
