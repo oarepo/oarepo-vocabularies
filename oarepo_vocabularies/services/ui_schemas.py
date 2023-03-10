6138ac3131b0faaa44b3da11691c0147ca68a77c
Mirek Simek <miroslav.simek@gmail.com>
2023-03-10 09:49:18 +0100

    ui schema

--- oarepo_vocabularies/services/ui_schemas.py
+++ oarepo_vocabularies/services/ui_schemas.py
@@ -3,7 +3,7 @@ from invenio_vocabularies.services.schema import i18n_strings
 from marshmallow import fields as ma_fields
 
 
-class HierarchySchema(ma.Schema):
+class HierarchyUISchema(ma.Schema):
     """HierarchySchema schema."""
 
     parent = ma_fields.String()
