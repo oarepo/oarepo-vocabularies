e5952ab4e843db69e15972d875f9531abd9c7988
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-04-14 17:18:13 +0200

    chore(search): :globe_with_meridians: add cs translations to vocab result item props

---
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/translationKeys.js
@@ -0,0 +1,11 @@
+// This file is used purely for translation keys extraction
+
+import { i18next } from "@translations/oarepo_vocabularies_ui/i18next";
+
+i18next.t('coarType')
+i18next.t('dataCiteType')
+i18next.t('alpha3CodeENG')
+i18next.t('alpha3CodeNative')
+i18next.t('ICO')
+i18next.t('acronym')
+i18next.t('nameType')
