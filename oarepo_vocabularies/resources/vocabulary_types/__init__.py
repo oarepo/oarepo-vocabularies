51b01be95e117cc426b4dd1bebc39150f2480cc9
Juraj Trappl <juraj.trappl@proton.me>
2023-06-30 14:01:14 +0200

    add more convenient import for vocab type resources

---
+++ oarepo_vocabularies/resources/vocabulary_types/__init__.py
@@ -0,0 +1,7 @@
+from oarepo_vocabularies.resources.vocabulary_types.config import VocabularyTypeResourceConfig
+from oarepo_vocabularies.resources.vocabulary_types.resource import VocabularyTypeResource
+
+__all__ = (
+    "VocabularyTypeResource",
+    "VocabularyTypeResourceConfig"
+)
