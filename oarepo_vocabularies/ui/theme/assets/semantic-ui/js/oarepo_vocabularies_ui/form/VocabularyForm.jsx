86b656e1667a5b46fc1a134016142262bde560c2
Dusanst <stojanovic@cesnet.cz>
2023-07-21 08:54:35 +0200

    cleaned up vocabularyForm

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/VocabularyForm.jsx
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/VocabularyForm.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { DetailPageEditForm } from "./DetailPageEditForm";
 import _has from "lodash/has";
 import _isEmpty from "lodash/isEmpty";
-import _mapValues from "lodash/mapValues";
-import _toPairs from "lodash/toPairs";
 import { useFormConfig } from "@js/oarepo_ui/forms";
 
 const options = {
@@ -14,10 +12,6 @@ const options = {
   ],
 };
 
-const translateObjectToArray = (obj) => {
-  return _toPairs(obj).map(([language, title]) => ({ language, name: title }));
-};
-
 const VocabularyForm = () => {
   const { record, formConfig, recordPermissions } = useFormConfig();
   console.log(record, formConfig, recordPermissions);
@@ -25,38 +19,16 @@ const VocabularyForm = () => {
   const editMode = _has(formConfig, "updateUrl");
   const hasPropFields = !_isEmpty(vocabularyProps);
   const apiCallUrl = editMode ? formConfig.updateUrl : formConfig.createUrl;
-  const availablePropFields = _mapValues(vocabularyProps.props, () => "");
-  console.log(availablePropFields);
-  const editModeAndProps = editMode
-    ? { ...availablePropFields, ...record.props }
-    : availablePropFields;
-
-  const propFieldsWithValues = hasPropFields ? editModeAndProps : {};
-
-  const initialValues = record;
-
-  // const initialValues = editMode
-  //   ? {
-  //       title: translateObjectToArray(record.title),
-  //       props: propFieldsWithValues,
-  //       id: record.id,
-  //     }
-  //   : {
-  //       title: [{ language: "cs", name: "" }],
-  //       props: propFieldsWithValues,
-  //       id: "",
-  //     };
 
   return (
     <DetailPageEditForm
-      initialValues={initialValues}
+      initialValues={record}
       formConfig={formConfig}
       options={options}
       hasPropFields={hasPropFields}
       vocabularyProps={vocabularyProps}
       apiCallUrl={apiCallUrl}
       editMode={editMode}
-      record={record}
     />
   );
 };
