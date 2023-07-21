bbe20437dd136c470c675c91012ed90415bd9d09
Juraj Trappl <juraj.trappl@proton.me>
2023-07-21 12:32:54 +0200

    create ext config specifically for auth resource ext

--- oarepo_vocabularies/authorities/ext.py
+++ oarepo_vocabularies/authorities/ext.py
@@ -19,8 +19,15 @@ class OARepoVocabulariesAuthorities(object):
     
     def init_app(self, app):
         """Flask application initialization."""
+        self.init_config(app)
         self.init_resource(app)
         app.extensions["oarepo-vocabularies-authorities"] = self
+        
+    def init_config(self, app):
+        from . import ext_config
+        
+        app.config.setdefault("OAREPO_VOCABULARIES_AUTHORITIES", ext_config.OAREPO_VOCABULARIES_AUTHORITIES)
+        app.config.setdefault("OAREPO_VOCABULARIES_AUTHORITIES_CONFIG", ext_config.OAREPO_VOCABULARIES_AUTHORITIES_CONFIG)
         
     def init_resource(self, app):
         """Initialize resources."""
