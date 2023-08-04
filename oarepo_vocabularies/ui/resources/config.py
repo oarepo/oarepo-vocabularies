a6e77229f81f8430b0ca2c678e799e966374e083
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-08-04 19:17:06 +0200

    provide ui_links for vocabularies

--- oarepo_vocabularies/ui/resources/config.py
+++ oarepo_vocabularies/ui/resources/config.py
@@ -1,12 +1,13 @@
 import marshmallow as ma
 from flask import current_app
 
+from oarepo_ui.resources.links import UIRecordLink
 from oarepo_ui.resources.config import RecordsUIResourceConfig
 from oarepo_vocabularies.ui.resources.components import (
     VocabularyRecordsComponent,
     DepositVocabularyOptionsComponent,
 )
-
+from invenio_records_resources.services import pagination_links, Link
 
 class InvenioVocabulariesUIResourceConfig(RecordsUIResourceConfig):
     template_folder = "../templates"
@@ -43,6 +44,19 @@ class InvenioVocabulariesUIResourceConfig(RecordsUIResourceConfig):
     components = [VocabularyRecordsComponent, DepositVocabularyOptionsComponent]
 
     request_vocabulary_type_args = {"vocabulary_type": ma.fields.Str()}
+
+    ui_links_item = {
+        "self": UIRecordLink("{+ui}{+url_prefix}/{vocabulary_type}/{id}"),
+        "edit": UIRecordLink("{+ui}{+url_prefix}/{vocabulary_type}/{id}/edit"),
+    }
+
+    @property
+    def ui_links_search(self):
+        return {
+            **pagination_links("{+ui}{+url_prefix}/{vocabulary_type}/{?args*}"),
+            "create": Link("{+ui}{+url_prefix}/{vocabulary_type}/_new"),
+        }
+
 
     def vocabulary_props_config(self, vocabulary_type):
         return current_app.config.get("INVENIO_VOCABULARY_TYPE_METADATA", {}).get(
