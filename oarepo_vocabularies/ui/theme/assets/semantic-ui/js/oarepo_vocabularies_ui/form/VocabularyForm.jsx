61f713e4f8f4eb10d58be41c5d4ff24eeda2bf88
ducica <dusanstojanovic256@gmail.com>
2023-10-18 14:19:14 +0200

    conditional rendering of propfields

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/VocabularyForm.jsx
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/VocabularyForm.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { DetailPageEditForm } from "./DetailPageEditForm";
 import _has from "lodash/has";
-import _isEmpty from "lodash/isEmpty";
 import { useFormConfig } from "@js/oarepo_ui/forms";
 
 const VocabularyForm = () => {
   const { record, formConfig } = useFormConfig();
   const { vocabularyProps } = formConfig;
   const editMode = _has(formConfig, "updateUrl");
-  const hasPropFields = !_isEmpty(vocabularyProps);
+  const hasPropFields = _has(vocabularyProps, "props");
 
   return (
     <DetailPageEditForm
