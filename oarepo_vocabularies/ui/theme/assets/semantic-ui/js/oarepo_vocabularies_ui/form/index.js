56db14ad29b12029ac36bd320184139ae3a0c917
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-10-24 16:37:25 +0200

    fix: fix js module entrypoint & component exports

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/index.js
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/index.js
@@ -1,13 +1,4 @@
-import { createFormAppInit } from "@js/oarepo_ui/forms";
-import VocabularyForm from "./VocabularyForm";
-
-export const overriddenComponents = {
-  "FormApp.layout": VocabularyForm,
-};
-
 export * from "./components";
 export * from "./VocabularyForm";
 export * from "./VocabularyFormSchema";
 export * from "./hooks";
-
-createFormAppInit(overriddenComponents);
