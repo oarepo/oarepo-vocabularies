9216bec44557e886b8fee73a550e2a635a36bdce
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-10-27 14:39:45 +0200

    add tooltip to feature button

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/FeaturedButton/FeaturedButton.jsx
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/FeaturedButton/FeaturedButton.jsx
@@ -10,12 +10,15 @@ export const FeaturedButton = () => {
   return (
     <Button
       fluid
+      toggle
+      active={isFeatured}
       disabled={isSubmitting}
       loading={isSubmitting}
       color="blue"
       icon="star"
       labelPosition="left"
-      content={i18next.t("Toggle featured")}
+      content={i18next.t("Feature")}
+      title={i18next.t('Item will be displayed on top of form input options')}
       type="button"
       onClick={() =>
         setFieldValue(
