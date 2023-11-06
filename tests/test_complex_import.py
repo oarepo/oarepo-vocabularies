da35dd895a36c9b2de997d110243db513f15989c
Mirek Simek <miroslav.simek@gmail.com>
2023-11-06 12:07:58 +0100

    tests work

--- tests/test_complex_import.py
+++ tests/test_complex_import.py
@@ -2,14 +2,17 @@ from pathlib import Path
 
 from invenio_vocabularies.records.api import Vocabulary
 from oarepo_runtime.datastreams.fixtures import load_fixtures
+from oarepo_runtime.datastreams.types import StatsKeepingDataStreamCallback
 
 
 def test_complex_import_export(app, db, cache, vocab_cf):
-    result = load_fixtures(
+    callback = StatsKeepingDataStreamCallback()
+    load_fixtures(
         Path(__file__).parent / "complex-data",
+        callback=callback,
     )
-    assert result.ok_count == 2737
-    assert result.failed_count == 0
-    assert result.skipped_count == 0
-    assert len(result.results) == 11
+
+    assert callback.ok_entries_count == 2737
+    assert callback.failed_entries_count == 0
+    assert callback.filtered_entries_count == 0
     Vocabulary.index.refresh()
