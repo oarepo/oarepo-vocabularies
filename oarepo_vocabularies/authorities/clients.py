9276f8c22fd9070811b59bb0d940c8d6e22559e2
Miroslav Bauer <mirekys@gmail.com>
2024-05-20 13:27:14 +0200

    feat(authorities): implement ror quicksearch with pagination

--- oarepo_vocabularies/authorities/clients.py
+++ oarepo_vocabularies/authorities/clients.py
@@ -1,6 +1,8 @@
 import json
 import requests
-from urllib.parse import urlencode, urljoin
+from urllib.parse import quote_plus, urljoin
+from invenio_records_resources.pagination import Pagination
+from requests import JSONDecodeError
 
 HTTP_OK = requests.codes["ok"]
 
@@ -16,12 +18,12 @@ class RORClientV2(object):
         self.api_url = url or "https://api.ror.org/v2/organizations"
         self.testing = testing
         if self.testing:
-           self.api_url = 'https://api.dev.ror.org/v2/organizations'
+            self.api_url = "https://api.dev.ror.org/v2/organizations"
 
         self.timeout = timeout or 10000
         self.page_size = page_size or 20
 
-    def quick_search(self, query):
+    def quick_search(self, query, size=10):
         """Search for ROR records matching the querystring.
 
         Performs a "quick search" of only the names and external_ids fields in ROR.
@@ -33,12 +35,33 @@ class RORClientV2(object):
         - Searching for external identifiers
         """
         headers = {"Accept": "application/json;charset=UTF-8"}
-        search_url = f"{self.api_url}?{urlencode(('query', query))}"
-    
-        search_result = json.loads(
-            requests.get(search_url, timeout=self.timeout, headers=headers)
-        )
+        query_params = {"query": quote_plus(query)}
+        current_page = Pagination(self.page_size, 1, size)
+
+        with requests.Session() as session:
+            while current_page and current_page.valid():
+                query_params["page"] = current_page.page
+
+                try:
+                    hits = session.get(
+                        self.api_url,
+                        timeout=self.timeout,
+                        headers=headers,
+                        params=query_params,
+                    ).json()
+                except ConnectionError as e:
+
+                    yield []
+
+                for item in hits["items"]:
+                    yield item
+
+                results_count = hits["number_of_results"]
+                if results_count < self.page_size:
+                    break
+
+                current_page = current_page.next_page if current_page.has_next else None
 
     def __repr__(self):
         """Create string representation of object."""
-        return "<RORClientV2: {0}>".format(self.url)
+        return "<RORClientV2: {0}>".format(self.api_url)
