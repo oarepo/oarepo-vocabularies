c16a1523cc5e246b893d2fcfa78d378cf9f116f9
Miroslav Bauer <mirekys@gmail.com>
2024-09-11 15:19:31 +0200

    refactor(remote): when forwarding ref, props must be spread over child!

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/VocabularyRemoteSelectField/VocabularyRemoteSelectModalTrigger.jsx
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/VocabularyRemoteSelectField/VocabularyRemoteSelectModalTrigger.jsx
@@ -1,21 +1,26 @@
 import * as React from "react";
-import { Icon, Button, Form } from "semantic-ui-react";
+import { Icon, Form } from "semantic-ui-react";
 import { i18next } from "@translations/oarepo_vocabularies_ui/i18next";
 import PropTypes from "prop-types";
 
 export const VocabularyRemoteSelectModalTrigger = React.forwardRef(
-  ({ label }, ref) => (
-    <Button
-      ref={ref}
-      className="array-field-add-button"
-      type="button"
-      icon
-      labelPosition="left"
-    >
-      <Icon name="add" />
-      {label}
-    </Button>
-  )
+  (props, ref) => {
+    const { label, ...rest } = props;
+
+    return (
+      <Form.Button
+        ref={ref}
+        className="array-field-add-button"
+        type="button"
+        icon
+        labelPosition="left"
+        {...rest}
+      >
+        <Icon name="add" />
+        {label}
+      </Form.Button>
+    );
+  }
 );
 
 VocabularyRemoteSelectModalTrigger.propTypes = {
