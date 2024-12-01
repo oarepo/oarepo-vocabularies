d23d45683115ab3b45c5eee25fb9ddf82e6fe6c2
pragerdom <dominik.prager@cesnet.cz>
2024-12-01 17:04:52 +0100

    feat: OpenAIRE Authority Provider implementation

--- tests/test_openaire.py
+++ tests/test_openaire.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 import pytest
-import os
 import jsonschema
 from oarepo_vocabularies.authorities import (
     AuthorityProvider,
@@ -18,7 +17,68 @@ def openaire_provider():
     assert isinstance(provider_object, AuthorityProvider)
     return provider_object
 
+def test_create_token(openaire_provider):
+    token = openaire_provider.openaire_client.get_token()
+    assert token is not None
+    
+def test_openaire_client_from_provider(openaire_provider):
+    assert openaire_provider.openaire_client
+    
+    search_query = "health"
+    access_token = openaire_provider.openaire_client.get_token()
+    assert access_token is not None
+    result = openaire_provider.openaire_client.quick_search(access_token, search_query)
+    assert result is not None
+    assert "response" in result
+    assert "results" in result["response"]
+    
+def test_openaire_provider_search_name(openaire_provider):
+    query = "Czech"
+    results = openaire_provider.search(identity=None, params={"q": query})
+    items, total = results  
+    assert total >= 1
+    assert len(items) >= 1
+    
+def test_openaire_provider_search_empty(openaire_provider):
+    query = ""
+    results = openaire_provider.search(identity=None, params={"q": query})
+    items, total = results
+    assert total == 0
+    assert len(items) == 0
+    
+def test_openaire_provider_pagination(openaire_provider):
+    query = "a"
+    results = openaire_provider.search(identity=None, params={"q": query})
+    items, total = results
+    assert total > 20
+    assert len(items) == 20
 
+    page2_results = openaire_provider.search(identity=None, params={"q": query, "page": 2})
+    page2_items, page2_total_value = page2_results
+    assert page2_total_value > 20
+    assert len(page2_items) == 20
+    
+    assert total <= page2_total_value
+    
+    for item in items:
+        if item is None:
+            continue
+        assert "title" in item
+        assert "funder" in item
+        assert "number" in item
+        assert "identifiers" in item
+        assert "organizations" in item
+        assert "program" in item
+        assert "acronym" in item
+        assert "tags" in item
+        assert "subjects" in item
+        assert "organizations" in item  
+
+def test_openaire_provider_get(openaire_provider):
+    item_id = "corda_______::89677d86a305a5b985427b92a81bc038"
+    item = openaire_provider.get(None, item_id)
+    assert item is not None
+    assert "title" in item
 
 def test_json_schema_validation(openaire_provider):
     with open(Path(__file__).parent/"schemas"/"award-v1.0.0.json") as f:
