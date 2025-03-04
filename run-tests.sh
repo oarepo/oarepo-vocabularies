268bc1669669be07c069c3ae1b20ca87f7bcd115
Mirek Simek <miroslav.simek@gmail.com>
2025-03-04 10:51:40 +0100

    Ignoring warnings

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,8 @@ OAREPO_VERSION="${OAREPO_VERSION:-12}"
 export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 
+export PYTHONWARNINGS="ignore"
+
 VENV=".venv"
 
 if test -d $VENV ; then
