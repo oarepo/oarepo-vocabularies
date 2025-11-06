a3301b71ec788d789f1a45e54ab0cd1ca8150dee
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2025-11-06 08:42:20 +0100

    using serializer for the multilingual field

--- oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/app.js
+++ oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/app.js
@@ -8,17 +8,111 @@ import {
 import { DepositFormApp, parseFormAppConfig } from "@js/oarepo_ui/forms";
 import React from "react";
 import { OARepoDepositSerializer } from "@js/oarepo_ui/api";
-
+import { RDMDepositRecordSerializer } from "@js/invenio_rdm_records/src/deposit/api/DepositRecordSerializer";
+import {
+  Field,
+  SchemaField,
+  AllowAdditionsVocabularyField,
+  FundingField,
+  RightsVocabularyField,
+  VocabularyField,
+} from "@js/invenio_rdm_records/src/deposit/serializers";
 import ReactDOM from "react-dom";
+
+// i18nField.js
+import _get from "lodash/get";
+import _set from "lodash/set";
+import _cloneDeep from "lodash/cloneDeep";
+
+export class I18nField extends Field {
+  constructor({ fieldpath, deserializedDefault = [], serializedDefault = {} }) {
+    super({ fieldpath, deserializedDefault, serializedDefault });
+  }
+
+  deserialize(record) {
+    const recordClone = _cloneDeep(record);
+    const rawValue = _get(
+      recordClone,
+      this.fieldpath,
+      this.deserializedDefault
+    );
+    console.log(record, "RECORDdawdwa");
+    if (!rawValue || typeof rawValue !== "object" || Array.isArray(rawValue)) {
+      return _set(recordClone, this.fieldpath, this.deserializedDefault);
+    }
+
+    const arrayValue = Object.entries(rawValue).map(([lang, value]) => ({
+      lang,
+      value,
+    }));
+    console.log(arrayValue, "ARRAY VALUE");
+
+    return _set(recordClone, this.fieldpath, arrayValue);
+  }
+
+  serialize(record) {
+    const recordClone = _cloneDeep(record);
+    const arrayValue = _get(
+      recordClone,
+      this.fieldpath,
+      this.serializedDefault
+    );
+
+    if (!Array.isArray(arrayValue)) {
+      return _set(recordClone, this.fieldpath, this.serializedDefault);
+    }
+
+    const objValue = {};
+    for (const { lang, value } of arrayValue) {
+      if (lang && value !== undefined && value !== null) {
+        objValue[lang] = value;
+      }
+    }
+
+    return _set(recordClone, this.fieldpath, objValue);
+  }
+}
 
 const recordSerializer = new OARepoDepositSerializer(
   ["errors", "expanded"],
   ["__key"]
 );
 
+class MyRDMDepositRecordSerializer extends RDMDepositRecordSerializer {
+  get depositRecordSchema() {
+    return {
+      title: new I18nField({ fieldpath: "title" }),
+    };
+  }
+  deserialize(record) {
+    let deserializedRecord = this._removeEmptyValues(record);
+    for (const key in this.depositRecordSchema) {
+      deserializedRecord = this.depositRecordSchema[key].deserialize(
+        deserializedRecord,
+        this.defaultLocale
+      );
+    }
+    return deserializedRecord;
+  }
+
+  serialize(record) {
+    let serializedRecord = this._removeEmptyValues(record);
+    for (const key in this.depositRecordSchema) {
+      serializedRecord = this.depositRecordSchema[key].serialize(
+        serializedRecord,
+        this.defaultLocale
+      );
+    }
+    return serializedRecord;
+  }
+}
+
 const { rootEl, config, ...rest } = parseFormAppConfig();
 const { overridableIdPrefix } = config;
-
+const rdmRecordSerializer = new MyRDMDepositRecordSerializer(
+  config.default_locale,
+  config.custom_fields.vocabularies
+);
 export const componentOverrides = {
   [`${overridableIdPrefix}.FormApp.layout`]: FormAppLayout,
   [`${overridableIdPrefix}.FormFields.container.awards`]:
@@ -35,7 +129,7 @@ ReactDOM.render(
   <DepositFormApp
     config={config}
     {...rest}
-    recordSerializer={recordSerializer}
+    recordSerializer={rdmRecordSerializer}
     componentOverrides={componentOverrides}
   />,
   rootEl
