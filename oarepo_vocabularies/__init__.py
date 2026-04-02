f4f9d28081792b269cde6bb893a6619bd229a425
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-04-02 08:57:11 +0200

    Major version bump (#255)

--- oarepo_vocabularies/__init__.py
+++ oarepo_vocabularies/__init__.py
@@ -10,6 +10,11 @@
 
 from __future__ import annotations
 
-__version__ = "3.0.0dev9"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("oarepo-vocabularies")
+except PackageNotFoundError:
+    __version__ = "0.0.0dev0+unknown"
 
 __all__ = ("__version__",)
